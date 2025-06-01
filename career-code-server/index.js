74449bdf279fb572bebf1283f4385cd4f7d4fdc9
Jaber Ahmed <jaber.ahmed4770@gmail.com>
2025-06-01 14:45:11 +0200

    jwt amr mathai jwt lagai dise.

--- career-code-server/index.js
+++ career-code-server/index.js
@@ -1,20 +1,46 @@
 const express = require('express');
 const app = express();
 const cors = require("cors");
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const port = process.env.PORT || 3000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 
 // middleware 
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials: true
+}));
 app.use(express.json());
+app.use(cookieParser());
 
 // server is coding...
 app.get('/', (req, res) => {
     res.send('Career code is coding...');
 })
 
+const logger = (req, res, next) => {
+    console.log('inside the logger middleware.');
+    next();
+}
+
+const verifyToken = (req, res, next) => {
+    const token = req.cookies.accessToken;
+    console.log("cookie in the middleware", token);
+    if (!token) {
+        return res.status(401).send({ message: "unautharized request." });
+    }
+
+    jwt.verify(token, process.env.JWT_ACCESS_SECRECT, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({message:'unautharised!'})
+        }
+        req.decoded = decoded
+        next();
+    })
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.61690px.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -36,6 +62,27 @@ async function run() {
 
         const jobCollection = client.db("careerCode").collection('jobs');
         const applicationCollection = client.db('careerCode').collection("applications");
+
+
+        // jwt token related api
+        app.post('/jwt', logger, verifyToken, async (req, res) => {
+            const userData = req.body;
+
+            const token = jwt.sign(userData, process.env.JWT_ACCESS_SECRECT, { expiresIn: "1h" });
+
+            // set token in the cookie
+            res.cookie('accessToken', token, {
+                httpOnly: true,
+                secure: false
+            })
+            console.log({ token })
+            res.send({ success: true });
+        })
+
+
+
+
+
 
 
         app.get('/jobs', async (req, res) => {
@@ -63,9 +110,15 @@ async function run() {
 
 
         // application related api
-        app.get('/applications', async (req, res) => {
+        app.get('/applications', logger, verifyToken, async (req, res) => {
+
             try {
                 const email = req.query.email;
+
+                if (email !== req.decoded.email) {
+                    return res.status(403).send({message:'forbidden access'})
+                }
+
                 let query = {};
                 if (email) {
                     query = { applicant: email };
@@ -76,7 +129,7 @@ async function run() {
                 res.status(500).send({ error: 'Failed to fetch applications', details: error.message });
             }
         });
-        
+
 
         app.post('/applications', async (req, res) => {
             const applicatin = req.body;
